fa8acdc12d08702a7c10dc6150b684257cf8b974
andrewcgaitskell <andrew@gaitskell.com>
2020-12-29 13:40:36 +0000

    Update steering_tutorial.py

--- tutorial/steering_tutorial.py
+++ tutorial/steering_tutorial.py
@@ -3,6 +3,8 @@ import time
 pwm = Adafruit_PCA9685.PCA9685() # Instantiate the object used to control the PWM
 pwm.set_pwm_freq(50) # Set the frequency of the PWM signal
 
+# port number was set at 3, this was wrong, it needs to be set to 2
+
 while 1:
   for i in range(0,100): # Slowly move the servo from 300 to 400
     pwm.set_pwm(2, 0, (300+i))
