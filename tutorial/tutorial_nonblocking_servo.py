9b9ca7d74f27dfd20e16e5530d08cb8f9e96e3d1
andrewcgaitskell <andrew@gaitskell.com>
2020-12-29 13:44:43 +0000

    Create tutorial_nonblocking_servo.py

---
+++ tutorial/tutorial_nonblocking_servo.py
@@ -0,0 +1,10 @@
+import RPIservo # Import a library that uses multiple threads to control the steering gear
+import time
+
+sc = RPIservo.ServoCtrl() # Instantiate the object that controls the steering gear
+
+sc.start() # Start this thread, when the servo does not move, the thread is suspended
+
+while 1:
+  sc.singleServo(3, -1, 2)
+  time.sleep(1)
