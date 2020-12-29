871414e5173a5052eb0cc9c60c867c6e69e8ce40
andrewcgaitskell <andrew@gaitskell.com>
2020-12-29 13:34:36 +0000

    Update servo_tutorial.py

--- tutorial/servo_tutorial.py
+++ tutorial/servo_tutorial.py
@@ -4,7 +4,9 @@ import time
 pwm = Adafruit_PCA9685.PCA9685() # Instantiate the object used to control the PWM
 pwm.set_pwm_freq(50) # Set the frequency of the PWM signal
 
-while 1: # Make the servo connected to the No. 3 servo port on the Motor HAT drive board reciprocate
+# Tutorial has Servo Port 3 - there are three servos and the ports start at 0? Changed to Port 1
+
+while 1: # Make the servo connected to the No. 1 servo port on the Motor HAT drive board reciprocate
   pwm.set_pwm(1, 0, 300)
   time.sleep(1)
   pwm.set_pwm(1, 0, 400)
