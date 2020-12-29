069006aadb05da146e7f45f7155100961aa55d1f
andrewcgaitskell <andrew@gaitskell.com>
2020-12-29 13:47:55 +0000

    Update tutorial_nonblocking_servo.py

--- tutorial/tutorial_nonblocking_servo.py
+++ tutorial/tutorial_nonblocking_servo.py
@@ -8,3 +8,7 @@ sc.start() # Start this thread, when the servo does not move, the thread is susp
 while 1:
   sc.singleServo(2, -1, 2)
   time.sleep(1)
+  sc.stopWiggle()
+  sc.singleServo(2, 1, 2)
+  time.sleep(1)
+  sc.stopWiggle()
