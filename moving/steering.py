28611895a510aba2a0070f11bd011c0fc1465832
andrewcgaitskell <andrew@gaitskell.com>
2020-12-31 14:26:35 +0000

    Update steering.py

--- moving/steering.py
+++ moving/steering.py
@@ -8,9 +8,11 @@ pwm.set_pwm_freq(50) # Set the frequency of the PWM signal
 z1,z2,z3 = z.zero_all()
 
 while 1:
-  for i in range(0,10): # Slowly move the servo from 300 to 400
+  for i in range(0,20): # Slowly move the servo from 300 to 400
+    print(z2+(i*10))
     pwm.set_pwm(2, 0, (z2+(i*10)))
-    time.sleep(0.05)
-  for i in range(0,10): # Slowly move the servo from 400 to 300
-    pwm.set_pwm(2, 0, ((z2+100)-(i*10)))
-    time.sleep(0.05)
+    time.sleep(0.5)
+  for i in range(0,20): # Slowly move the servo from 400 to 300
+    print((z2+200)-(i*10))
+    pwm.set_pwm(2, 0, ((z2+200)-(i*10)))
+    time.sleep(0.5)
