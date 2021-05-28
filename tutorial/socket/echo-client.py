297639ebc72db3783bc153812cb450c5824bdcbb
andrewcgaitskell <andrew@gaitskell.com>
2021-05-28 10:44:26 +0100

    Update echo-client.py

--- tutorial/socket/echo-client.py
+++ tutorial/socket/echo-client.py
@@ -2,10 +2,16 @@
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+serverMACAddress = 'B4:E6:2D:97:8B:0F'
+port = 1 # this was 3, it works as 1
+#s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+#s.connect((serverMACAddress,port))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+HOST = serverMACAddress  # The server's hostname or IP address
+PORT = 1        # The port used by the server
+
+with socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
     data = s.recv(1024)
