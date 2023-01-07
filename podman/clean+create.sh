ca564e2703fcc7e930c81687b3ec87b08395e761
andrewcgaitskell <andrew@gaitskell.com>
2023-01-07 14:48:46 +0000

    Update clean+create.sh

--- podman/clean+create.sh
+++ podman/clean+create.sh
@@ -14,6 +14,7 @@ podman build -f Dockerfile -t image_python_frontend_1 .
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
+--privileged \
 --name container_flask_frontend_1 \
 --user $uid:$gid \
 -p 0.0.0.0:5000:5000  \
