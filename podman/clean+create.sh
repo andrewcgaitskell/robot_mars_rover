fc76c66f70c6555a06cd0deaf14afa89708334f9
andrewcgaitskell <andrew@gaitskell.com>
2023-01-07 14:01:52 +0000

    Update clean+create.sh

--- podman/clean+create.sh
+++ podman/clean+create.sh
@@ -10,24 +10,12 @@ subuidSize=$(( $(podman info --format "{{ range \
 subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
-podman pod create \
---name pod_flask_frontend \
---infra infra_flask_frontend \
---network bridge \
---uidmap 0:1:$uid \
---uidmap $uid:0:1 \
---uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
---gidmap 0:1:$gid \
---gidmap $gid:0:1 \
---gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
---publish 5050:5050
-
 podman build -f Dockerfile -t image_python_frontend_1 .
 
 ##-v /HOST-DIR:/CONTAINER-DIR
 
 podman run -dt \
 --name container_flask_frontend_1 \
---pod pod_flask_frontend \
 --user $uid:$gid \
+--publish 5050:5050
 localhost/image_python_frontend_1:latest
