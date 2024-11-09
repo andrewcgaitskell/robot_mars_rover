eb59f56aac9c15f060e4657c03fc31993b0b6cd6
andrewcgaitskell <andrew@gaitskell.com>
2024-11-09 21:01:59 +0000

    Update app.py

--- quart_app/app.py
+++ quart_app/app.py
@@ -1,8 +1,12 @@
 import random
 import asyncio
 from quart import Quart, render_template, jsonify
+from .blueprints.line_following import line_following_bp
 
-app = Quart(__name__)
+app = Quart(__name__, templates_folder="templates")
+
+# Register the sensor blueprint
+app.register_blueprint(line_following_bp, url_prefix='/line')
 
 # Endpoint to provide random data
 @app.route('/data')
